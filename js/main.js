851bb18805b1f1deb3df9f54ebf2bb9b379dcbef
Benjamin Oakes <hello@benjaminoakes.com>
2015-04-19 15:04:00 -0500

    Move "people" code to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+function userTemplate(user) {
+  var buffer = [];
+  buffer.push('<a href="' + user.html_url + '">');
+  buffer.push('<h1>' + user.login + '</h1></a>');
+  buffer.push('<img src="' + user.avatar_url + '" width=150 height=150/>');
+  buffer.push('<hr>');
+  return buffer.join('');
+}
 
+$(function () {
+  $.getJSON('https://api.github.com/orgs/techcorridorio/public_members?callback=?', function (response) {
+    var html = response.data.map(userTemplate);
+    $('#people').html(html);
+  });
+});
