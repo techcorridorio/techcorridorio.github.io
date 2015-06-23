caa223bdfdd112ad3e899b061b632217b1d5d4ad
caseywatts <casey.s.watts@gmail.com>
2015-06-23 16:01:44 -0400

    move presentation logic to a presenter

--- js/meetup-events.js
+++ js/meetup-events.js
@@ -6,40 +6,6 @@ var Meetup = function(meetupURL, maxEvents) {
 	// If maxEvents==0, show all.
 	this.maxEvents = (typeof maxEvents!=='undefined') ? maxEvents : 0;
 
-	var printDate = function(date) {
-		// Pretty print the event date
-		var day = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-		var months = [
-			"January", "February", "March", "April",
-			"May", "June", "July", "August",
-			"September", "October", "November", "December"
-		];
-
-		return day[date.getDay()]  + ', '
-			+ months[date.getMonth()] + ' '
-			+ date.getDate() + ', '
-			+ date.getFullYear()
-			+ ' at '
-			+ (date.getHours() % 12) + ':'
-			+ (date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes())
-			+ ' '
-			+ (date.getHours() < 12 ? 'AM' : 'PM');
-	}
-
-	var printVenue = function(venue) {
-		// Pretty print the event venue
-		var template = '<a href="https://www.google.com/maps/place/'
-			+ encodeURI(venue.address_1) + '+'
-			+ encodeURI(venue.city) + '" target="_blank">'
-			+ venue.name
-			+ '</a>';
-        return template
-	}
-
-	var createEventPanel = function(event) {
-		// Generates a Bootstrap panel for each event
-	}
-
 	this.getEvents = function(callback) {
 		$.ajax({
 			url: this.meetupURL,
