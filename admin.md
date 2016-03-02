4b89542d70b2e61c8940d0b934f493d4d0c68018
Benjamin Oakes <hello@benjaminoakes.com>
2016-03-01 21:55:48 -0600

    Move welcome script to website for easier access

---
+++ admin.md
@@ -0,0 +1,10 @@
+---
+layout: page
+header: Admin
+---
+
+Tools for administrators.
+
+* [Welcome](javascript:browserWelcome())
+
+<script src="js/welcome.js"></script>
---
+++ js/welcome.js
@@ -0,0 +1,54 @@
+function assert(boolean) {
+  if (!boolean) { throw "assertion failed!"; }
+}
+
+function welcome(usernames) {
+  var mentions, last, formattedUsernames;
+
+  mentions = usernames.map(function (username) {
+    if (username.match(/^@/)) {
+      return username;
+    } else {
+      return '@' + username;
+    }
+  });
+
+  if (mentions.length > 1) {
+    last = mentions.pop();
+    mentions.push('and ' + last);
+  }
+
+  if (mentions.length > 2) {
+    formattedUsernames = mentions.join(', ');
+  } else {
+    formattedUsernames = mentions.join(' ');
+  }
+
+  return 'Welcome ' + formattedUsernames + '!  Would you like to give a short introduction to the group?  For example, what tech you enjoy, where you work, etc.';
+}
+
+function test() {
+  assert(welcome(['foo']).match('Welcome @foo!'));
+  assert(welcome(['foo', 'bar']).match('Welcome @foo and @bar!'));
+  assert(welcome(['foo', 'bar', 'baz']).match('Welcome @foo, @bar, and @baz!'));
+}
+
+function browserWelcome() {
+  var raw, names;
+  
+  raw = prompt('Names?  (separate with whitespace)');
+  names = raw.split(/\s+/);
+
+  prompt('Copy', welcome(names));
+}
+
+if (typeof this.process !== 'undefined') { // node.js
+  var names = process.argv.slice(2, process.argv.length); // first one is the filename :S
+
+  if (names.length == 0) {
+    test();
+    console.log('Tests pass');
+  } else {
+    console.log(welcome(names));
+  }
+}
